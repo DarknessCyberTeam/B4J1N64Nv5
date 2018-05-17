551d67c2dc99614a49eb1115e59dbc9a6adb7aea
DarknessCyberTeam <38211186+DarknessCyberTeam@users.noreply.github.com>
2018-05-17 08:39:13 +0000

    Add files via upload

---
+++ module/cinta.py
@@ -0,0 +1,32 @@
+qty=int(input("Berapa Persen Cintamu ke aku?"))
+jumlah=qty
+if jumlah > 100 :
+    keterangan="kamu sangat mencintai aku, Kamu mau gk jadi pendamping hidup aku?"
+elif jumlah > 75 and jumlah <= 100:
+	keterangan="Kamu cinta sama aku, kamu mau gk jadi pendamping hidup aku?"
+elif jumlah > 46 and jumlah <= 75:
+	keterangan="kamu sederhana dalam mencintai, kamu mau gk jadi pendamping hidup aku?"
+elif jumlah > 20 and jumlah <= 46:
+	keterangan="kamu benci aku, tolong jangan benci sama aku"
+elif jumlah > 0 and jumlah <= 20:
+	keterangan="kamu sangat benci aku, :'-("
+print ("Jumlah=", jumlah)
+print ("Keterangan=", keterangan)
+print ("========================")
+print ("JANGAN MERINDUKAN AKU,BERAT!!! KAMU GK BAKAL KUAT, BIAR AKU AJAH :v ")
+print ("========================")
+print ("by.MR.B4J1N64N")
+print ("========================")
+print ("     #        #            ############   #             #      #######          #        #    #########    #        #            # # #  # # # # ")  
+print ("     #        #            #          #    #           #       #                 #      #     #       #    #        #          #      ##        # ")
+print ("     #        #            #          #     #         #        #                  #    #      #       #    #        #          #                # ")
+print ("     #        #            #          #      #       #         #                   #  #       #       #    #        #           #              # ")
+print ("     #        #            #          #       #     #          #######              ##        #       #    #        #             #          # ")
+print ("     #        #            #          #        #   #           #                    ##        #       #    #        #               #      # ")
+print ("     #        #            #          #         # #            #                    ##        #       #    #        #                 #  # ") 
+print ("     #        ########     ############          #             #######              ##        #########     ########                   ## ")
+print ("========================") 
+print ("SAVE PROGRAMER GALAU")
+print ("")
+
+pio=int(input("tekan enter untuk mematikan"))
---
+++ module/r.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python
+
+import time
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+print ("saat lu berdua sama cewe lu di tempat sepi")
+print ("apa yang akan lu lakuin?")
+ 
+print"+-------------------+"
+print"+1. Ngajak Ngaji    +"
+print"+                   +"
+print"+2. di eue :v       +"
+print"+                   +"
+print"+-------------------+"
+num1 = raw_input("pilih mhank : ")
+if num1 == "1":
+    print"Semoga Masuk Surga:) "
+elif num1 == "2":
+    print"Otak Bokep Njing:v"
+else:
+    print"kuy gelud tod:*"
+
+
+while True:
+ print bcolors.WARNING + ("Tobat Lah Nak Kiamat Udah dekat:)") + bcolors.ENDC
+
